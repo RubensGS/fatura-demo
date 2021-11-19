f306b045b759c4b416ab799235f68a1e6b7b5217
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-11-19 13:10:32 -0300

    Refactoring lombok: Classe Total

--- src/main/java/com/demo/entities/resp/Total.java
+++ src/main/java/com/demo/entities/resp/Total.java
@@ -1,30 +1,14 @@
 package com.demo.entities.resp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class Total {
 
     private Double totalDebitos;
     private Double totalCreditos;
 
-    public Total() { }
-
-    public Total(Double totalDebitos, Double totalCreditos) {
-        this.totalDebitos = totalDebitos;
-        this.totalCreditos = totalCreditos;
-    }
-
-    public Double getTotalDebitos() {
-        return totalDebitos;
-    }
-
-    public void setTotalDebitos(Double totalDebitos) {
-        this.totalDebitos = totalDebitos;
-    }
-
-    public Double getTotalCreditos() {
-        return totalCreditos;
-    }
-
-    public void setTotalCreditos(Double totalCreditos) {
-        this.totalCreditos = totalCreditos;
-    }
 }
