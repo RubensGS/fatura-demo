ca47f74fa7cb60ed1ea8dc036e8151ff49335d74
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-11-19 14:30:55 -0300

    Ajuste fluxo na instanciação das listas nacionais e internacionais

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -4,7 +4,6 @@ import com.demo.entities.resp.Total;
 import com.demo.entities.resp.Transacoes;
 import com.demo.payloads.RequestDAO;
 import com.demo.payloads.ResponseDAO;
-import com.demo.services.TransacaoService;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -13,31 +12,18 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 
 import java.io.File;
 import java.io.IOException;
-
-
 public class JavaApplication {
 
 	public static void main(String[] args) {
-
-		Fatura fatura;
-		DadosCliente dadosCliente;
-		Transacoes transacoes;
-		TransacaoService transacaoService = new TransacaoService();
-		Total total;
 
 		ObjectMapper mapper = new ObjectMapper();
 
 		try {
 			RequestDAO req = mapper.readValue(new File("src/main/resources/request.json"), RequestDAO.class);
 
-			dadosCliente = new DadosCliente(req.getConta().getNomeCliente(), req.getTransacoes());
-
-			transacoes = new Transacoes(
-					transacaoService.getListaInternacional(req.getTransacoes()),
-					transacaoService.getListaNacional(req.getTransacoes()));
-
-			total = new Total(dadosCliente.getTotalFatura(), dadosCliente.getSaldoAnterior());
-			fatura = new Fatura(dadosCliente, transacoes, total);
+			DadosCliente dadosCliente = new DadosCliente(req.getConta().getNomeCliente(), req.getTransacoes());
+			Total total = new Total(dadosCliente.getTotalFatura(), dadosCliente.getSaldoAnterior());
+			Fatura fatura = new Fatura(dadosCliente, new Transacoes(req.getTransacoes()), total);
 
 			ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
 			writer.writeValue(new File("src/main/resources/response.json"), new ResponseDAO(fatura));
--- src/main/java/com/demo/entities/resp/Transacoes.java
+++ src/main/java/com/demo/entities/resp/Transacoes.java
@@ -9,10 +9,37 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor @AllArgsConstructor @Getter @Setter
+@NoArgsConstructor @Setter @AllArgsConstructor @Getter
 public class Transacoes {
 
     private List<Transacao> internacionais = new ArrayList<>();
     private List<Transacao> nacionais = new ArrayList<>();
 
+    public Transacoes(List<Transacao> list) {
+        this.internacionais = getListaInternacional(list);
+        this.nacionais = getListaNacional(list);
+    }
+
+    public List<Transacao> getListaInternacional(List<Transacao> list) {
+        List<Transacao> aux = new ArrayList<>();
+        for (Transacao transacao : list ) {
+            if(transacao.getTipo().equals("Internacional")) {
+                transacao.setTipo("");
+                aux.add(transacao);
+            }
+        }
+        return aux;
+    }
+
+    public List<Transacao> getListaNacional(List<Transacao> list) {
+        List<Transacao> aux = new ArrayList<>();
+
+        for (Transacao transacao : list ) {
+            if(transacao.getTipo().equals("Nacional")) {
+                transacao.setTipo("");
+                aux.add(transacao);
+            }
+        }
+        return aux;
+    }
 }
