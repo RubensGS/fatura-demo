dbecf67da3102556301349f7f5b1ee9638123893
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-11-19 12:36:33 -0300

    Revert "Adicionando Lombok no projeto + ajustes na classe Conta"
    This reverts commit 17749b28455aa9ff69095ef9e2d63c120b4d7010.

--- src/main/java/com/demo/entities/req/Conta.java
+++ src/main/java/com/demo/entities/req/Conta.java
@@ -1,11 +1,38 @@
 package com.demo.entities.req;
 
-import lombok.*;
-
-@NoArgsConstructor @AllArgsConstructor @Getter @Setter @ToString
 public class Conta {
 	
 	private String nomeCliente;
 	private Double limite;
 
+	public Conta() { }
+
+	public Conta(String nomeCliente, Double limite) {
+		this.nomeCliente = nomeCliente;
+		this.limite = limite;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public Double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(Double limite) {
+		this.limite = limite;
+	}
+
+	@Override
+	public String toString() {
+		return "Conta{" +
+				"nomeCliente='" + nomeCliente + '\'' +
+				", limite=" + limite +
+				'}';
+	}
 }
