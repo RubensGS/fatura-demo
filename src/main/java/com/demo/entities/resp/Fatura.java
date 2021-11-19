69b6d0a4406a027a59983cea4f614a3a59766624
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-11-19 13:10:02 -0300

    Refactoring lombok: Classe Fatura

--- src/main/java/com/demo/entities/resp/Fatura.java
+++ src/main/java/com/demo/entities/resp/Fatura.java
@@ -1,40 +1,13 @@
 package com.demo.entities.resp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class Fatura {
     private DadosCliente dadosCliente;
     private Transacoes transacoes;
     private Total totais;
-
-    public Fatura() {  }
-
-    public Fatura(DadosCliente dadosCliente, Transacoes transacoes, Total totais) {
-        this.dadosCliente = dadosCliente;
-        this.transacoes = transacoes;
-        this.totais = totais;
-    }
-
-    public DadosCliente getDadosCliente() {
-        return dadosCliente;
-    }
-
-    public void setDadosCliente(DadosCliente dadosCliente) {
-        this.dadosCliente = dadosCliente;
-    }
-
-    public Transacoes getTransacoes() {
-        return transacoes;
-    }
-
-    public void setTransacoes(Transacoes transacoes) {
-        this.transacoes = transacoes;
-    }
-
-    public Total getTotais() {
-        return totais;
-    }
-
-    public void setTotais(Total totais) {
-        this.totais = totais;
-    }
-
 }
