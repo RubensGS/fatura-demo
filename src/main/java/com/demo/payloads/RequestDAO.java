c96deee4a164cb8f87e5d9e7af62af7c9aee14b7
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2021-11-19 13:04:48 -0300

    Refactoring lombok: Classe RequestDAO

--- src/main/java/com/demo/payloads/RequestDAO.java
+++ src/main/java/com/demo/payloads/RequestDAO.java
@@ -2,35 +2,16 @@ package com.demo.payloads;
 
 import com.demo.entities.req.Conta;
 import com.demo.entities.req.Transacao;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class RequestDAO {
      private Conta conta;
      private List<Transacao> transacoes = new ArrayList<>();
-
-    public RequestDAO() { }
-
-    public RequestDAO(Conta conta, List<Transacao> transacoes) {
-        this.conta = conta;
-        this.transacoes = transacoes;
-    }
-
-    public Conta getConta() {
-        return conta;
-    }
-
-    public void setConta(Conta conta) {
-        this.conta = conta;
-    }
-
-    public List<Transacao> getTransacoes() {
-        return transacoes;
-    }
-
-    public void setTransacoes(List<Transacao> transacoes) {
-        this.transacoes = transacoes;
-    }
-
 }
